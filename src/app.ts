8207a12d474fadb769b0dc4e4cf6eebf2e6bff53
Rime <rimeislam672@gmail.com>
2024-08-24 21:18:36 +0600

    user interfase

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
-import express, {NextFunction, Request, Response} from 'express';
+import express, {NextFunction, Application, Request, Response} from 'express';
 import cors from 'cors';
+import cookieParser from "cookie-parser";
 
-const app = express()
 
+const app: Application = express()
 app.use(cors());
-
 app.use(express.json());
+app.use(cookieParser());
 
 
 app.get('/', (req: Request, res: Response) => {
---
+++ src/app/modules/User/user.constant.ts
@@ -0,0 +1,4 @@
+export const user_role = {
+    admin: "admin",
+    user: "user"
+} as const;
